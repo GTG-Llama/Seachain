51d9443c0172fcb327c9acfb5029c70f43f7ab88
GTG-Llama <harrywu1243@gmail.com>
2023-10-14 20:43:17 +0800

    Update the news scraping to scrape multiple news in the onion news website

--- scrape_news.py
+++ scrape_news.py
@@ -1,8 +1,12 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Function to scrape news article
+########
+# General function to scrape news article (not quite working for any site cuz this html is not specific to the websites)
+########
+
 # def scrape_article(url):
 #     # Fetch the page content
 #     response = requests.get(url)
@@ -27,67 +31,27 @@ import csv
 #     }
 
 
-def scrape_article(url):
-    # Fetch the page content
-    response = requests.get(url)
-    if response.status_code != 200:
-        return None
-    
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Extract the title and author
-    title = soup.select_one('h1#main-heading').text if soup.select_one('h1#main-heading') else "N/A"
-    author = soup.select_one('div.ssrcss-68pt20-Text-TextContributorName').text if soup.select_one('div.ssrcss-68pt20-Text-TextContributorName') else "N/A"
-    
-    # Extract the main body text by looping through each text block
-    body_text_blocks = soup.select('div.ssrcss-11r1m41-RichTextComponentWrapper p')
-    body = ' '.join([block.text for block in body_text_blocks])
-    
-    return {
-        'Title': title,
-        'Author': author,
-        'URL': url,
-        'Body': body
-    }
+###################################################
 
-
-# URL to scrape
-url_to_scrape = "https://www.bbc.com/news/world-middle-east-67040221"
-
-# Scrape the article
-article_data = scrape_article(url_to_scrape)
-
-# Output the scraped data to the console
-print("Scraped Data:")
-print(article_data)
-
-# Save to CSV if scraping was successful
-if article_data:
-    keys = ['Title', 'Author', 'URL', 'Body']
-    with open('article_data.csv', 'w', newline='') as output_file:
-        writer = csv.DictWriter(output_file, fieldnames=keys)
-        writer.writeheader()
-        writer.writerow(article_data)
-#     print("CSV file generated: article_data.csv")
-
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
+########
+# Scrape single BBC News
+#########
 
 # def scrape_article(url):
-#     print(f"Scraping {url}...")
+#     # Fetch the page content
 #     response = requests.get(url)
 #     if response.status_code != 200:
-#         print("Failed to fetch article.")
 #         return None
-
+    
 #     soup = BeautifulSoup(response.text, 'html.parser')
+    
+#     # Extract the title and author
 #     title = soup.select_one('h1#main-heading').text if soup.select_one('h1#main-heading') else "N/A"
 #     author = soup.select_one('div.ssrcss-68pt20-Text-TextContributorName').text if soup.select_one('div.ssrcss-68pt20-Text-TextContributorName') else "N/A"
+    
+#     # Extract the main body text by looping through each text block
 #     body_text_blocks = soup.select('div.ssrcss-11r1m41-RichTextComponentWrapper p')
 #     body = ' '.join([block.text for block in body_text_blocks])
-    
-#     print(f"Scraped title: {title}")
     
 #     return {
 #         'Title': title,
@@ -96,27 +60,108 @@ if article_data:
 #         'Body': body
 #     }
 
-# def get_recent_articles(main_url, num_articles=10):
-#     print(f"Fetching recent articles from {main_url}...")
-#     response = requests.get(main_url)
-#     if response.status_code != 200:
-#         print("Failed to fetch main page.")
-#         return None
+###################################################
 
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     article_links = soup.select('a.block-link__overlay-link')[:num_articles]  # Replace 'YOUR_ARTICLE_LINK_SELECTOR'
-#     article_urls = ['https://www.bbc.com' + link['href'] for link in article_links]
+######
+# function to scrape single onion news
+######
+
+def scrape_article(url):
+    # Fetch the page content
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
     
-#     print(f"Fetched {len(article_urls)} article URLs.")
+    soup = BeautifulSoup(response.text, 'html.parser')
     
-#     return article_urls
+    # Extract the title, author, and main body text
+    # Note: These selectors are hypothetical. You'll need to inspect the actual HTML to find the correct ones.
+    # title = soup.select_one('h1.title').text if soup.select_one('h1.title') else "N/A"
+    # author = soup.select_one('span.author').text if soup.select_one('span.author') else "N/A"
+    title = soup.select_one('h1.sc-1efpnfq-0.dAlcTj').text if soup.select_one('h1.sc-1efpnfq-0.dAlcTj') else "N/A"
+    author = soup.select_one('div.ssrcss-68pt20-Text-TextContributorName').text if soup.select_one('div.ssrcss-68pt20-Text-TextContributorName') else "N/A"
+    body_text_blocks = soup.select('p.sc-77igqf-0.fnnahv')
+    body = ' '.join([block.text for block in body_text_blocks]) if body_text_blocks else "N/A"
+    
+    return {
+        'Title': title,
+        'Author': author,
+        'URL': url,
+        'Body': body
+    }
+###################################################
 
-# # Main URL of BBC
-# main_url = 'https://www.bbc.com/'
+#####
+# Run the function to get content from one url
+#####
 
-# # Get first 10 recent article URLs
-# recent_articles = get_recent_articles(main_url, 10)
+# # URL to scrape
+# url_to_scrape = "https://www.theonion.com/pros-and-cons-of-keeping-senile-politicians-in-office-1850900125"
 
+# # Scrape the article
+# article_data = scrape_article(url_to_scrape)
+
+# # Output the scraped data to the console
+# print("Scraped Data:")
+# print(article_data)
+
+
+# # Get the directory of the current script
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# # Create the path for the new CSV file to be in the same directory as the script
+# csv_path = os.path.join(script_dir, 'article_data.csv')
+
+# # Save to CSV if scraping was successful
+# if article_data:
+#     keys = ['Title', 'Author', 'URL', 'Body']
+#     with open(csv_path, 'w', newline='') as output_file:
+#         writer = csv.DictWriter(output_file, fieldnames=keys)
+#         writer.writeheader()
+#         writer.writerow(article_data)
+#     print("CSV file generated: article_data.csv")
+
+
+###################################################
+#######
+# function to get urls of multiple news articles given main news website
+#######
+
+def get_recent_articles(main_url, num_articles=10):
+    print(f"Fetching recent articles from {main_url}...")
+    response = requests.get(main_url)
+    if response.status_code != 200:
+        print("Failed to fetch main page.")
+        return None
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    article_links = soup.select('div.sc-cw4lnv-5.dYIPCV a.sc-1out364-0.dPMosf.js_link')[:num_articles]  # Replace 'YOUR_ARTICLE_LINK_SELECTOR'
+    article_urls = [link['href'] for link in article_links]
+    
+    print(f"Fetched {len(article_urls)} article URLs.")
+    
+    return article_urls
+
+# Main URL of BBC
+main_url = 'https://www.theonion.com/politics?startIndex=20'
+
+# Get first 100 recent article URLs
+recent_articles = get_recent_articles(main_url, 100)
+
+# print all the article links that will be scraped
+print()
+print("Total", len(recent_articles), "articles fetched.", "Below are the urls:")
+print(recent_articles)
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Create the path for the new CSV file to be in the same directory as the script
+csv_path = os.path.join(script_dir, 'all_article_data.csv')
+
+#####
+# scraping all the articles and output to csv
+#####
 # if recent_articles:
 #     # Initialize a list to hold all articles data
 #     all_articles_data = []
@@ -129,10 +174,11 @@ if article_data:
 
 #     # Save all scraped data to CSV
 #     keys = ['Title', 'Author', 'URL', 'Body']
-#     with open('all_articles_data.csv', 'w', newline='') as output_file:
+#     with open(csv_path, 'w', newline='') as output_file:
 #         writer = csv.DictWriter(output_file, fieldnames=keys)
 #         writer.writeheader()
 #         writer.writerows(all_articles_data)
 #     print("CSV file generated: all_articles_data.csv")
 # else:
 #     print("No article URLs fetched.")
+###################################################
